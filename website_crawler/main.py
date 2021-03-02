c15d763b57bf71724f935b0175a5939006913a38
Chris <chris@hopfen.space>
2021-03-02 01:47:29 +0100

    Implemented a major part of the crawler

--- website_crawler/main.py
+++ website_crawler/main.py
@@ -2,12 +2,14 @@
 
 import os
 import queue
+import typing
 import logging
 import argparse
 import threading
 import html.parser
+import urllib.parse
 
-
+import requests
 
 USER_AGENT_STRING = "Mozilla/5.0 (compatible; WebsiteCrawler)"
 QUEUE_ACCESS_TIMEOUT = 1
@@ -36,77 +38,161 @@ class HyperlinkSearcher(html.parser.HTMLParser):
                 )
             ))
 
-    def get(self) -> list:
-        return self.hyperlinks
-
 
 class Downloader:
     """
     Downloader for website content, filtering duplicates, extracting more targets
+
+    :param website: base URI of the website which should be downloaded
+    :param target: target directory where to store downloaded files
+    :param logger: logger used to keep track of various events
     """
 
-    def __init__(self, website: str, target: str, logger: logging.Logger):
+    def __init__(
+            self,
+            website: str,
+            target: str,
+            logger: logging.Logger
+    ):
         self.website = website
         self.target = target
         self.logger = logger
+        self.netloc = urllib.parse.urlparse(self.website).netloc
 
         if not os.path.exists(target):
             os.makedirs(target, exist_ok=True)
+            self.logger.debug("Created missing target directory.")
         elif not os.path.isdir(target):
             logger.critical("Target directory is no directory!")
             raise RuntimeError("Target directory is no directory!")
 
-        self.downloads = []
-        self.runners = []
+        self.runners = {}
+        self.downloads = {}
         self.queue = queue.Queue()
 
-        self.queue.put(website)
+        # A runner's state may be one of the following five options:
+        # 0 -> the runner has just been created, it's not running yet
+        # 1 -> the runner is up and performing actual work
+        # 2 -> the runner is doing something, but it was requested to quit
+        # 3 -> the runner exited successfully
+        # 4 -> the runner crashed due to an exception
+        self._runner_states = {}
 
-    def start_runner(self, suffix: str):
+        self.queue.put(website)
+        self.logger.debug("Initialized downloader.")
+
+    def start_runner(self, key: str) -> bool:
         """
         Start a runner in a separate thread
 
-        :param suffix: identification suffix added to the logger name
+        :param key: identification key for the runner (and suffix of its logger)
+        :return: whether a new runner has been added successfully
         """
 
-        r = _Runner(suffix, self)
-        self.runners.append(r)
-        threading.Thread(target=r.run, daemon=False).start()
+        if key in self.runners:
+            self.logger.error(f"Couldn't add runner '{key}' as it's already there.")
+            return False
 
+        r = threading.Thread(target=self._run, args=(key,), daemon=False)
+        self.runners[key] = r
+        self._runner_states[key] = 0
+        self.logger.debug(f"Added runner '{key}'.")
+        r.start()
 
-class _Runner:
-    """
-    Worker to perform the actual task of downloading, extracting, storing
-    """
-
-    def __init__(self, suffix: str, downloader: Downloader):
-        self.downloader = downloader
-        self.queue = self.downloader.queue
-        self.logger = self.downloader.logger.getChild(suffix)
-
-        self.started = False
-        self.running = False
-
-    def stop(self):
+    def stop_runner(self, key: str, timeout: int) -> bool:
         """
-        Stop the runner (which will just finish the last job before exiting)
+        Join the specified runner (which is a blocking operation)
+
+        :param key: identification key for the runner (and suffix of its logger)
+        :param timeout: max time to wait for the worker thread to finish
+        :return: whether the runner was found and told to stop
         """
 
-        self.running = False
+        if key not in self.runners:
+            self.logger.warning(f"Runner '{key}' couldn't be stopped: not found.")
+            return False
 
-    def run(self):
+        self._runner_states[key] = 2
+        self.runners[key].join(timeout=timeout)
+        return True
+
+    def _store(self, path: str, content: typing.Union[bytes, str]):
+        """
+        Store the specified content at the location 'path' on disk
+
+        :param path: original path on the remote side
+        :param content: result of the request for that path
+        """
+
+        pass
+
+    def _handle(self, url: str, logger: logging.Logger) -> typing.List[str]:
+        """
+        Retrieve the content of the given URL, store it and extract more targets
+
+        :param url: target URL which should be downloaded and analysed
+        :param logger: logger for this particular job
+        :return: list of other, yet unknown URLs found in the content
+        """
+
+        logger.debug(f"Currently processing: {url}")
+        request = requests.get(url, headers={"User-Agent": USER_AGENT_STRING})
+        code = request.status_code
+
+        if not code == 200:
+            logger.error(f"Received code {code} for {url}. Skipping.")
+            self.downloads[url] = code
+            return []
+
+        self.downloads[url] = code
+        searcher = HyperlinkSearcher(logger)
+        searcher.feed(request.text)
+
+        self._store(urllib.parse.urlparse(url).path, request.content)
+
+        # Ensure that no cross-site references are added
+        result = []
+        for ref in searcher.hyperlinks:
+            if ref.startswith("#"):
+                continue
+            new_reference = urllib.parse.urljoin(self.website, ref)
+            if urllib.parse.urlparse(new_reference).netloc == self.netloc:
+                result.append(new_reference)
+
+        return result
+
+    def _run(self, ident: str):
         """
         Perform the actual work in a loop
+
+        :param ident: (hopefully) unique string for runner identification
         """
 
-        self.logger.debug(f"Starting running loop for {self} ...")
+        logger = self.logger.getChild(ident)
+        logger.debug(f"Starting running loop for {ident} ...")
+        self._runner_states[ident] = 1
 
-        self.started = True
-        self.running = True
+        while self._runner_states[ident] < 2:
+            try:
+                current_job = self.queue.get(True, QUEUE_ACCESS_TIMEOUT)
+            except queue.Empty:
+                logger.debug("Queue was empty.")
+                continue
 
-        while self.running and not self.queue.empty():
-            next_job = self.queue.get(True, QUEUE_ACCESS_TIMEOUT)
-            pass  # TODO
+            # Avoid duplicates in the queue by reserving the downloads 'slot'
+            if current_job in self.downloads:
+                continue
+            self.downloads[current_job] = 0
+
+            try:
+                for item in self._handle(current_job, logger):
+                    self.queue.put(item)
+            except:
+                logger.error(f"Error during handling of '{current_job}'!", exc_info=True)
+                self._runner_states[ident] = 4
+                raise
+
+        self._runner_states[ident] = 3
 
 
 def setup() -> argparse.ArgumentParser:
